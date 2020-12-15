07b420f79833036fd6944fcaeeaa4b1347eac1b1
hamzahfirman <hamzahfirman08@email.arizona.edu>
2020-12-14 21:49:03 -0700

    New Push: API completed

---
+++ db.js
@@ -0,0 +1,57 @@
+'use strict';
+
+require('dotenv').config();
+const { Pool } = require('pg');
+
+
+const postgreConnectionString =
+    `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}`;
+
+console.log(postgreConnectionString);
+
+const postgrePool = new Pool({
+    connectionString: process.env.DATABASE_URL ? process.env.DATABASE_URL : postgreConnectionString,
+    ssl: { rejectUnauthorized: false }
+});
+
+function getPlaces() {
+    return postgrePool.query('select * from nearbyplaces.place')
+    .then(result => {
+        console.log(result);
+        if (result.rows) {
+            return result.rows;
+        } else {
+            throw Error('The places could not be found in the database.');
+        }
+    });
+}
+
+function savePlace(name, address, category, phone, hours) {
+    return postgrePool.query('INSERT into nearbyplaces.place (name, address, category, phone, hours, town, state, totalreview, price, website, link) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) returning id', [name, address, category, phone, hours, town, state, totalreview, price, website, link])
+        .then(x => x.rows);
+}
+
+function saveReview(username, rating, comment, placeid) {
+    return postgrePool.query('INSERT into nearbyplaces.review (username, rating, comment, placeid) values ($1, $2, $3, $4) returning id', [username, rating, comment, placeid])
+        .then(x => x.rows);
+}
+
+function addTotalReview(placeid){
+    return postgrePool.query('UPDATE nearbyplaces.place set totalreview = totalreview + 1 where placeid = $1', [placeid])
+    .then(x => x.rows);
+}
+
+
+function getSearchedPlace(place, town) {
+    return postgrePool.query('SELECT * from nearbyplaces.place where name like $1 and town like $2', [place, town])
+        .then(result => {
+            console.log(result);
+            if (result.rows) {
+                return result.rows;
+            } else {
+                throw Error('The place is not found in the database.');
+            }
+        });
+}
+
+module.exports = { getPlaces, savePlace, saveReview, getSearchedPlace, addTotalReview }
--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 
 const express = require('express');
 const app = express();
+var db = require('./db');
 var cors = require('cors'); // This gives access to public (whoever wants to use this API)
 var data = require('./data');
 var bodyParser = require('body-parser');
@@ -24,21 +25,18 @@ app.get('/' , (request, response) => {
 })
 // METHOD: GET
 app.get('/places' , (request, response) => {
-    response.json(data.businessesInStates);
+    db.getPlaces().then(x => response.json(x));
 })
 // METHOD: GET
 app.get('/search/:searchTerm/:location' , (request, response) => {
-let searchBusiness = request.params.searchTerm;
-let searchLocation = request.params.location;
 
-let foundBusiness = data.businessesInStates.businesses.find(x => ((x.town.toLowerCase() === searchLocation.toLowerCase()) 
-&& (x.name.toLowerCase() === searchBusiness.toLowerCase())));
-if(foundBusiness) {
-    response.json(foundBusiness);
-}
-else{
-    response.status(404).json({error: `The searched info could not be found.`})
-}
+    let searchBusiness = request.params.searchTerm;
+    let searchLocation = request.params.location.substring(0,1).toUpperCase() + 
+    request.params.location.substring(1).toLowerCase();
+
+
+    db.getSearchedPlace(searchBusiness+"%", searchLocation+"%").then(x => response.json(x));
+
 
 })
 // METHOD: POST
@@ -58,38 +56,23 @@ app.post('/place', (request, response) =>{
     let website   = request.body.website;
     let totalReviews    = request.body.totalReviews;
 
-    //Stores the given data from a user in JSON 
-    data.scores.push({
-        name:name, 
-        address: address + ", " + town + ", " + state + " " + zip,
-        categories: categories,
-        cost: cost,
-        hours: hours,
-        link: link,
-        place: place,
-        reviews: reviews,
-        type: type,
-        website: website,
-        totalReviews: totalReviews
-    });
-    response.json({message:'a new business added successfully.'})
+    //Stores the given data from a user using a query function in db.js to 
+    // 'place' table in postgres
+    db.savePlace(name, "" + address +", " + town + ", " + state + ", " + zip, type, phone, hours
+    , town, state, totalReviews, cost, website, link).then(x => response.json({message: "The place is succesfully added."}))
 });
 
 // METHOD: POST
 app.post('/review/:placeId', (request, response) =>{
     let username = request.body.username;
     let review   = request.body.review;
-    let placeId  = request.params.placeId;
-
-    let aBusiness = data.businessesInStates.businesses.find(x => (x.id === Number(placeId)));
-
-
-    aBusiness.reviews.push({
-        username: username,
-        review: review
-    });
-    response.json({message:`a new review for ${aBusiness.name} added successfully.`})
-
+    let placeid  = request.params.placeId;
+    let rating   = request.params.rating;
+    
+    db.addTotalReview(placeid).then(x => response.json({message: "A new total review."}));
+    //Stores the given data from a user using a query function in db.js to 
+    // 'place' table in postgres
+    db.saveReview(username, rating, review, placeid).then(x => response.json({message: "The review is succesfully added for this place."}))
 
 });
 
